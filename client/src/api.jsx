544f35297570f557bbdfe6349f9d1aa44c75e102
suaib <suaib8211@gmail.com>
2024-06-01 22:25:18 +0530

    final  commit

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchAlerts = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/api/data');
+    const response = await axios.get('https://security-alerts-dashboard.onrender.com/api/data');
     return response.data;
   } catch (error) {
     console.error('Error fetching data: ', error);
